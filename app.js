7287e0f401a951c970c284c95b9cb9ff2eace59b
r-v-g-7 <r.vinayak.garg77@gmail.com>
2025-12-15 16:34:11 +0530

    Updated instructions in Readme

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const { PORT_NUMBER } = require("./utils/constants");
+require("dotenv").config();
 
 const app = express();
 
---
+++ config.js
@@ -0,0 +1,3 @@
+const PORT_NUMBER = process.env.PORT || 3000;
+
+module.exports = { PORT_NUMBER }
