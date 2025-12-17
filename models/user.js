2b6f012db7a7f5b2af3fe15efb78652d89b05b34
r-v-g-7 <r.vinayak.garg77@gmail.com>
2025-12-17 17:38:59 +0530

    user model upate

--- models/user.js
+++ models/user.js
@@ -1,10 +1,42 @@
-const Schema = mongoose.Schema;
-const ObjectId = schema.ObjectId;
-
-const UserSchema = new Schema({
+const mongoose = require("mongoose")
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
-    }
-})
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        select: false,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["student", "professor"]
+    },
+    fieldOfStudy: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    institution: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
 
