190afafa3f40349652bbda06408218cc839b16dc
r-v-g-7 <r.vinayak.garg77@gmail.com>
2025-12-23 05:27:11 +0530

    /profile/show  api

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,13 +1,36 @@
 const express = require("express")
 const isTokenValid = require("../middleware/auth");
 const useFindUserByUserId = require("../utils/useFindUserWithUserId");
+const sendResponse = require("../utils/response");
 
 const profileRouter = express.Router();
 
 profileRouter.get("/show", isTokenValid, async (req, res, next) => {
 
-    const user = await useFindUserByUserId(req.userId)
-    res.send(user)
+    try {
+        const user = await useFindUserByUserId(req.userId)
+        if (!user) {
+            sendResponse(res, 401, false, "User NOT found, kindly login again");
+        }
+        const { name, fieldOfStudy, institution, role } = user;
+        sendResponse(res, 201, true, "Profile fetched Successfully", { name, role, fieldOfStudy, institution });
+    } catch (err) {
+        sendResponse(res, 400, false, "Something went wrong, User NOT found");
+    }
+})
+
+profileRouter.patch("/update", isTokenValid, async (req, res, next) => {
+
+    try {
+        const user = await useFindUserByUserId();
+        if (!user) {
+            sendResponse(res, 401, false, "User NOT found, kindly login again");
+        }
+        const { name, fieldOfStudy, institution } = user;
+    } catch (err) {
+        sendResponse(res, 400, false, "Something went wrong, User NOT found")
+    }
+
 
 })
 
