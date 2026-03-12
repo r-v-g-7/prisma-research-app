5eec4075b5218849f0ee230d146d56d0e775d524
r-v-g-7 <r.vinayak.garg77@gmail.com>
2026-03-12 10:29:43 +0530

    just a small change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173", // frontend port
+    origin: 'http://localhost:5173',
     credentials: true
 }));
 
